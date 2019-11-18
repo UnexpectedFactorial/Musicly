1016a619133c350dc0f26338169c0acd611e5576
Reke <reke.ogbokor@gmail.com>
2019-11-18 11:27:08 -0800

    added ranking template and footer for the homepage

--- index.php
+++ index.php
@@ -1,61 +1,174 @@
-<?php include 'view/header.php';?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Musicly</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<script src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+			
+		<nav class="navbar navbar-expand-lg navbar-dark static-top">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#"><img src="">Musicly</a>
+				<button class="navbar-toggler" type="button">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<button type="button" class="btn btn-outline-primary btn-sm mr-1">Sign in</button>
+						</li>
+						<li class="nav-item">
+							<button type="button" class="btn btn-primary btn-sm">Create account</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 
-	<div id="slides" class="carousel slide" data-ride="carousel">
-		<div class="carousel-inner">
-			<div class="carousel-item active">
-				<img src="images/concert.jpg">
-				<div class="carousel-caption">
-					<h1 class="display-1">Music for everyone</h1>
-					<h3>Thousands of songs. No credit card needed.
-					</h3>
+		<div id="slides" class="carousel slide" data-ride="carousel">
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="images/concert.jpg">
+					<div class="carousel-caption">
+						<h1 class="display-1">Music for everyone</h1>
+						<h3>Thousands of songs. No credit card needed.
+						</h3>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<form class="form-inline active-purple-4 d-flex justify-content-center mt-4">
-  		<input class="form-control form-control-lg mr-3 w-75" type="text" placeholder="Search for artists, tracks"
-    aria-label="Search">
-  		<i class="fas fa-search" aria-hidden="true"></i>
-	</form>
+		<form class="form-inline active-purple-4 d-flex justify-content-center mt-5">
+	  		<input class="form-control form-control-lg mr-3 w-75" type="text" placeholder="Search for artists, tracks"
+	    aria-label="Search">
+	  		<i class="fas fa-search" aria-hidden="true"></i>
+		</form>
 
-	<h4 class="text-center trend mt-4">Hear what's trending in the Musicly community</h4>
+		<h4 class="text-center trend mt-5">Hear what's trending in the Musicly community</h4>
 
-	<div class="container">
-		<div class="row text-center mt-4">
-   			<div class="col">
-      			1 of 5
-   			</div>
-   			<div class="col">
-      			2 of 5
-   			</div>
-   			<div class="col">
-      			3 of 5
-   			</div>
-   			<div class="col">
-      			4 of 5
-   			</div>
-   			<div class="col">
-      			5 of 5
-   			</div>
+		<div class="container">
+			<div class="row text-center mt-5">
+	   			<div class="col pr-1">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
+	   			<div class="col pr-1">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
+	   			<div class="col pr-1">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
+	   			<div class="col pr-1">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
+	   			<div class="col pr-1">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
 			</div>
-		<div class="row text-center mt-4">
-   			<div class="col">
-      			1 of 5
-   			</div>
-   			<div class="col">
-      			2 of 5
-   			</div>
-   			<div class="col">
-      			3 of 5
-   			</div>
-   			<div class="col">
-      			4 of 5
-   			</div>
-   			<div class="col">
-      			5 of 5
-   			</div>
-   		</div>
-	</div>
+			<div class="row text-center mt-4">
+	   			<div class="col pr-1">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
+	   			<div class="col pr-1 mb-4">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
+	   			<div class="col pr-1">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
+	   			<div class="col pr-1">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
+	   			<div class="col pr-1">
+	      			<img src="images/imageholder.png">
+	      			<div>
+	      				<a href="#">Music Title</a>
+	      			</div>
+	      			<div>
+	      				<a href="#">Music Artist</a>
+	      			</div>
+	   			</div>
+	   		</div>
+		</div>
+
+		<div class="container-fluid py-4" style="height: 300px; background-color: #f2f2f2;">
+			<div class="text-center my-3">
+				<h1 class="display-5">Thanks for listening. Now join in.</h1>
+			</div>
+			<div class="text-center mb-4">
+				<h4>Upload your favorite tracks</h4>
+			</div>
+			<div class="text-center mb-3">
+				<button type="button" class="btn btn-primary btn-lg">Create account</button>
+			</div>
+			<div class="text-center mb-5">
+				Already have an account?
+			<button type="button" class="btn btn-primary btn-md text-center d-inline" style="background-color: #fff; color: #000;">Sign in</button>
+			</div>
+		</div>
+
+		<footer style="background-color: black; color: white;">
+  			<div class="footer-copyright text-center py-3">© 2019 Copyright:
+    			Musicly
+  			</div>
+		</footer>
+	</body>
+</html>
+
 
 	
 
