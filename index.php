363ab928f904fe5d1cee40403d34f30fc08f36a3
Reke <reke.ogbokor@gmail.com>
2019-11-18 08:57:38 -0800

    editted navbar to put it on top of concert.jpg and imported a few files

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <?php include 'view/header.php';?>
 
 	<div id="slides" class="carousel slide" data-ride="carousel">
-		<ul class="carousel-indicators">
-			<li data-target="#slides" data-slide-to="0" class="active"></li>
-			<li data-target="#slides" data-slide-to="1"></li>
-		</ul>
 		<div class="carousel-inner">
-			<div class="item active">
+			<div class="carousel-item active">
 				<img src="images/concert.jpg">
-				<div class="carousel-caption">
-					<h1 class="display-1">Music for everyone</h1>
-					<h3>Thousands of songs. No credit card needed.
-					</h3>
-				</div>
-			</div>
-			<div class="carousel-item">
-				<img src="nextmusic.jpg">
 				<div class="carousel-caption">
 					<h1 class="display-1">Music for everyone</h1>
 					<h3>Thousands of songs. No credit card needed.
@@ -25,7 +13,49 @@
 		</div>
 	</div>
 	<form class="form-inline active-purple-4 d-flex justify-content-center mt-4">
-  		<input class="form-control form-control-lg mr-3 w-75" type="text" placeholder="Search for artists and tracks"
+  		<input class="form-control form-control-lg mr-3 w-75" type="text" placeholder="Search for artists, tracks"
     aria-label="Search">
   		<i class="fas fa-search" aria-hidden="true"></i>
 	</form>
+
+	<h4 class="text-center trend mt-4">Hear what's trending in the Musicly community</h4>
+
+	<div class="container">
+		<div class="row text-center mt-4">
+   			<div class="col">
+      			1 of 5
+   			</div>
+   			<div class="col">
+      			2 of 5
+   			</div>
+   			<div class="col">
+      			3 of 5
+   			</div>
+   			<div class="col">
+      			4 of 5
+   			</div>
+   			<div class="col">
+      			5 of 5
+   			</div>
+			</div>
+		<div class="row text-center mt-4">
+   			<div class="col">
+      			1 of 5
+   			</div>
+   			<div class="col">
+      			2 of 5
+   			</div>
+   			<div class="col">
+      			3 of 5
+   			</div>
+   			<div class="col">
+      			4 of 5
+   			</div>
+   			<div class="col">
+      			5 of 5
+   			</div>
+   		</div>
+	</div>
+
+	
+
--- view/header.php
+++ view/header.php
@@ -3,26 +3,28 @@
 		<head>
 			<title>Musicly</title>
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<script src="https://kit.fontawesome.com/effe52d6f7.js" crossorigin="anonymous"></script>
-			<script src="carousel.js"></script>
-			<link href="css/style.css" rel="stylesheet">
+			<link href="css/bootstrap.min.css" rel="stylesheet">
+			<link href="css/bootstrap.css" rel="stylesheet">
+			<script src="js/bootstrap.min.js"></script>
 		</head>
 		<body>
-			<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+			
+			<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
 				<div class="container-fluid">
 					<a class="navbar-brand" href="#"><img src="">Musicly</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+					<button class="navbar-toggler" type="button">
 						<span class="navbar-toggler-icon"></span>
 					</button>
 					<div class="collapse navbar-collapse" id="navbarResponsive">
 						<ul class="navbar-nav ml-auto">
 							<li class="nav-item">
-								<button type="button" class="btn btn-outline-primary mr-1">Sign in</button>
+								<button type="button" class="btn btn-outline-primary btn-sm mr-1">Sign in</button>
 							</li>
 							<li class="nav-item">
-								<button type="button" class="btn btn-primary">Create account</button>
+								<button type="button" class="btn btn-primary btn-sm">Create account</button>
 							</li>
 						</ul>
 					</div>
 				</div>
 			</nav>
+
